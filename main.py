6bcaad8518dbecb3add5fd4e00719175bb428d0f
alecwood7 <alec.hv.wood@gmail.com>
2022-08-14 17:18:05 -0500

    Nav bar work and additional site pages

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
 if __name__ == "__main__":
     app.run(debug=False)
