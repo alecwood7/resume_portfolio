263d7cbceb6f8e27a4055f2120badb359b752321
alecwood7 <alec.hv.wood@gmail.com>
2022-08-15 10:40:16 -0500

    Additional linking of site pages and logic.

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def home():
 def resume():
     return render_template("resume.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=False)
